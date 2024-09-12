08fb8065704d89b1ad1e0a0060206db1beff440c
Mehdi <mehdi.tch@icloud.Com>
2024-09-12 11:11:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -108,6 +108,12 @@
 //     alert("nom incorrect")
 // }
 
+
+
+
+
+
+
 // function calculerIMC(poids, taille) {
 //     taille = taille / 100
 //     return poids / (taille * taille)
@@ -119,44 +125,169 @@
 
 // alert("votre imc est de :" + " " + imc.toFixed(2))
 
-for (let i = 1; i <= 10; i++){
-    console.log(i)
-}
 
-let i = 10
-while (i >= 0) {
-    console.log(i)
-    i--
-}
 
-for (let i = 0; i <= 20; i += 2) {
-    console.log(i)
-}
+                                                                // LES BOUCLES ///
 
-function somme(n) {
-    let total = 0
-    for (let i = 1; i <= n; i++) {
-        total += i
-    }
-    return total
-}
 
-console.log(somme(10))
 
-function multiplication(n) {
-    for (let i = 1; i <= 10; i++) {
-        console.log (n + " x " + i + " = " + (n*i))
-    }
-}
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
 
-multiplication(123456789);
 
-let tableau = ["mehdi", "med", "meh"]
-for (let i = 0; i < tableau.length; i++) {
-    console.log(tableau[i])
-}
 
-let tableauu = ["mehdii", "medd", "mehh"]
-for (let prenom of tableauu) {
-    console.log(prenom)
-}
+
+
+
+
+// let i = 10
+// while (i >= 0) {
+//     console.log(i)
+//     i--
+// }
+
+
+
+
+
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+// function somme(n) {
+//     let total = 0
+//     for (let i = 1; i <= n; i++) {
+//         total += i
+//     }
+//     return total
+// }
+
+// console.log(somme(10))
+
+
+
+
+
+
+
+// function multiplication(n) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log (n + " x " + i + " = " + (n*i))
+//     }
+// }
+
+// multiplication(123456789);
+
+
+
+
+
+
+
+// let tableau = ["mehdi", "med", "meh"]
+// for (let i = 0; i < tableau.length; i++) {
+//     console.log(tableau[i])
+// }
+
+
+
+
+
+
+
+// let tableauu = ["mehdii", "medd", "mehh"]
+// for (let prenom of tableauu) {
+//     console.log(prenom)
+// }
+
+
+
+
+
+
+
+// let tableauuu = ["mehdii", "medd", "mehh"]
+// for (let prenom in tableauuu) {
+//     console.log(prenom + ": " + tableauuu[prenom])
+// }
+
+
+
+
+                                                                    // LES FONCTIONS //
+
+
+
+
+// function direBonjour(mehdi) {
+//     return `Bonjour, ${mehdi} !`
+// }
+
+
+
+
+
+
+
+// function calculatrice(a, b, operateur) {
+//     if (operateur === "+") {
+//         return a + b;
+//     } else if (operateur === "-") {
+//         return a - b;
+//     } else if (operateur === "*") {
+//         return a * b;
+//     } else if (operateur === "/") {
+//         if (b === 0) {
+//             return "Erreur: Division par zéro"
+//         } else {
+//             return a / b
+
+//         }
+//     } else {
+//         return "Opérateur non valide"
+//     }
+// }
+
+// console.log(calculatrice(12, 2, "-"))
+// console.log(calculatrice(8, 8, "+"))
+// console.log(calculatrice(10, 4, "*"))
+// console.log(calculatrice(14, 2, "/"))
+
+
+
+
+// function puissance(nombre, exposant) {
+//     return nombre ** exposant
+// }
+
+// console.log(puissance(2, 3))
+
+
+
+
+
+// function estPair(nombre) {
+//     return nombre % 2 === 0;
+//   }
+
+//   console.log(estPair(9))
+//   console.log(estPair(8))
+
+
+
+
+
+// function calculerTVA(prixHT, tauxTVA) {
+//     return prixHT * (1 + tauxTVA / 100)
+//   }
+  
+// console.log(calculerTVA(100, 20))
+// console.log(calculerTVA(50, 5))
