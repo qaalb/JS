12d07f85a26c288e5400a35446ff9fd9e6810667
Mehdi <mehdi.tch@icloud.Com>
2024-11-07 08:24:23 +0100

    Add files via upload

---
+++ exercice3promesse.js
@@ -0,0 +1,11 @@
+function verifierUtilisateur(){}
+const Promesse = new Promise((resolve, reject) => {
+        const age;
+        if (age => 18) {resolve("Utilisateur majeur");} 
+            else { reject("Utilisateur mineur");}
+})
+
+.then(text => {console.log(text);})
+.catch(texte => {console.log(texte);})
+console.log(Promesse);
+console.log("hello monde")
